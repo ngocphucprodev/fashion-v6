c272cf8d56dbacb99b05cc8ce15f6cdd117817b8
ngoncphucprodev <ngocphuc1399@gmail.com>
2023-04-10 16:59:14 +0700

    first commit

--- server/src/controller/ordersController.js
+++ server/src/controller/ordersController.js
@@ -22,6 +22,17 @@ const OrdersController = {
 
     store(req, res, next) {
         console.log(req.body)
+        res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+        // Request methods you wish to allow
+        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+        // Request headers you wish to allow
+        res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+        // Set to true if you need the website to include cookies in the requests sent
+        // to the API (e.g. in case you use sessions)
+        res.setHeader('Access-Control-Allow-Credentials', true);
         const order = new Orders(req.body)
         order.save(req.body)
             .then((data) =>
